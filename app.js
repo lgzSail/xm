3866df69076ceaae6f48a61ad2249bf2f32bc5b2
lgz <48312111+lgzSail@users.noreply.github.com>
2019-04-10 18:16:29 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+var server=express();
+server.listen(3001);
+server.use(express.static('./public'));
+
+const bodyParser=require('body-parser');
+server.use(bodyParser.urlencoded({
+  entended:false
+}))
+
+const origin=require('./routers/origin.js');
+server.use('/index',origin);
